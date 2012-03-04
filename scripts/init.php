7baf85a97181d690da0d54f29637c984e38f411b
menthol <commit@menthol-foundation.com>
2012-03-04 16:21:48 +0100

    Need require once in init.php script to avoid multi inclusion of classes.

--- scripts/init.php
+++ scripts/init.php
@@ -7,16 +7,16 @@ if (!class_exists('ClassLoader\\ClassLoader')) {
   require dirname(__DIR__) . '/src/ClassLoader.php';
 }
 if (!class_exists('Extensions\\ClassLoader\\Cache\\Cache')) {
-  require dirname(__DIR__) . '/ext/ClassLoader/Cache/Cache.php';
+  require_once dirname(__DIR__) . '/ext/ClassLoader/Cache/Cache.php';
 }
 if (!class_exists('Extensions\\ClassLoader\\Cache\\Instance')) {
-  require dirname(__DIR__) . '/ext/ClassLoader/Cache/Instance.php';
+  require_once dirname(__DIR__) . '/ext/ClassLoader/Cache/Instance.php';
 }
 if (!class_exists('Extensions\\ClassLoader\\PathBuilder\\PathBuilder')) {
-  require dirname(__DIR__) . '/ext/ClassLoader/PathBuilder/PathBuilder.php';
+  require_once dirname(__DIR__) . '/ext/ClassLoader/PathBuilder/PathBuilder.php';
 }
 if (!class_exists('Extensions\\ClassLoader\\PathBuilder\\SplPathBuilder')) {
-  require dirname(__DIR__) . '/ext/ClassLoader/PathBuilder/SplPathBuilder.php';
+  require_once dirname(__DIR__) . '/ext/ClassLoader/PathBuilder/SplPathBuilder.php';
 }
 
 ClassLoader::initWithPathBuilder(SplPathBuilder::initWithNamespace('Extensions', dirname(__DIR__) . '/ext'));
